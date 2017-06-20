795a44f9dc5a5fc6442761141bd1c80bf88a7bc6
Benjamin Sanchez-Lengeling <beangoben@gmail.com>
2017-06-20 02:50:29 -0400

    better package

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import re
+import ast
 from setuptools import setup
 
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('qwx_test.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 setup(name='qwxwhat',
-      version='0.1',
+      version=version,
       description='QuantumWorldX tests',
       url='',
+      py_modules=['qwxwhat'],
+      install_requires=['pythonwhat'],
       author='',
       author_email='',
       license='MIT',
